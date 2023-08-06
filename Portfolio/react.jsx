52ee424ae807016ee288b7fc944bb716fd038790
Henrique da Silveira <139990659+imrickss@users.noreply.github.com>
2023-08-05 22:39:44 -0300

    Add files via upload

---
+++ Portfolio/react.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./style.css";
+
+export const IphoneSe = () => {
+  return (
+    <div className="iphone-SE">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="varinha" alt="Varinha" src="varinha-1.png" />
+          <img className="icon-menu" alt="Icon menu" src="icon-menu.png" />
+          <img
+            className="criando-experincias"
+            alt="Criando experincias"
+            src="criando-experi-ncias-e-realizando-pedidos.svg"
+          />
+          <p className="text-wrapper">
+            Olá, me chamo Henrique Ataide e Curso Desenvolvimento de sistemas SESI/SC e busco oportunidades como
+            desenvolvedor Full-Stack ou UX/UI.
+          </p>
+          <div className="overlap">
+            <div className="div">GitHub</div>
+            <img className="github" alt="Github" src="github-2.png" />
+          </div>
+          <div className="overlap-2">
+            <div className="text-wrapper-2">Linkedin</div>
+            <img className="image" alt="Image" src="image-1.png" />
+          </div>
+          <div className="overlap-3">
+            <div className="rectangle" />
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <div className="rectangle-4" />
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="ellipse" />
+            <img className="vector" alt="Vector" src="vector-1.svg" />
+            <img className="img" alt="Vector" src="vector-2.svg" />
+            <div className="rectangle-8" />
+            <div className="text-wrapper-3">
+              {"{"}
+              {"}"}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
